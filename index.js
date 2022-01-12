ab2600a317e1bdfd4e329cdff37589eae92b5c8f
Luis Morales <lmorales@ultimamillasa.com.ar>
2022-01-12 10:46:44 -0300

    ednpointt para address

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var router = express.Router();              // Instancia de Express router
 const processTxs = async function(txs, latest = false) {
   let transactions = [];
   if(txs.length > 0){
-    txs.forEach(async tx => {
+    for (const tx of txs) {
       if(latest){
         if(transactions.length < 10){
 	        var t = await web3.eth.getTransaction(tx);
@@ -28,20 +28,54 @@ const processTxs = async function(txs, latest = false) {
         transactions.push(await web3.eth.getTransaction(tx));
       }
       return transactions;
-    });
+    }
   }else{
     return transactions
   }
 }
-
-// GET http://localhost:8080/api/blocks
+router.get('/accounts', async function(req, res){
+  await web3.eth.getAccounts(function (error, result){
+    if(!error){
+      res.json({
+        status: 'success',
+        res
+      });
+    }else{
+      res.json({
+        status: 'error',
+        error,
+      })
+    }
+    
+  });
+});
+router.get('/transfer/:from/:to/:amount', function(req, res){
+  const transactionObject = {
+    from: req.params.from,
+    to: req.params.to,
+    value: 100000000000
+  }
+  web3.eth.sendTransaction(transactionObject, function (error, result){
+    if(!error){
+      res.json({
+        status: 'success',
+        result,
+      })
+    }else{
+      console.error(error);
+    }
+  });
+});
+// GET http://localhost:8080/api/v1/latest
 router.get('/latest', function(req, res) {
+  console.log('latest');
     const n = 10;
     let blocks = [];
     let txs = [];
     web3.eth.getBlockNumber(async function(error, result){
       if (!error){
         var latestBlock = result;
+        console.log(latestBlock);
         for (var i = 0; i < n; i++) {
           var block = await web3.eth.getBlock(latestBlock - i);
           // Block data
